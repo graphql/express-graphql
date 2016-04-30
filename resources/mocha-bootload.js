d3d170c52b38494261e6f4af32963d92f2c68523
Ben Kinsesy <bkinsey@gmail.com>
2016-04-29 23:09:48 -1000

    Make dependencies up-to-date, needed to fix bug https://phabricator.babeljs.io/T2212

--- resources/mocha-bootload.js
+++ resources/mocha-bootload.js
@@ -8,8 +8,9 @@
  */
 /* eslint-disable no-console */
 
-require('babel/register')({
-  optional: [ 'es7.asyncFunctions' ]
+
+require('babel-core/register')({
+  plugins: [ 'transform-async-to-generator', 'transform-runtime' ]
 });
 
 process.on('unhandledRejection', function (error) {
