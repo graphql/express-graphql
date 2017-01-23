ddcdc4e19294edbd5c659427549f875b5fc3116a
Greg Hurrell <greg@hurrell.net>
2017-01-23 14:04:56 -0800

    Fix a comment typo

--- src/renderGraphiQL.js
+++ src/renderGraphiQL.js
@@ -18,7 +18,7 @@ type GraphiQLData = {
 // Current latest version of GraphiQL.
 const GRAPHIQL_VERSION = '0.8.0';
 
-// Ensures string values are save to be used within a <script> tag.
+// Ensures string values are safe to be used within a <script> tag.
 function safeSerialize(data) {
   return data ? JSON.stringify(data).replace(/\//g, '\\/') : 'undefined';
 }
