5a082b2a1d02aadb1aa5a284e61f03aaf7fc37de
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-02-19 00:16:20 +0200

    Polyfill 'Promise' to support IE10/11
    Based on https://github.com/graphql/graphiql/pull/189

--- src/renderGraphiQL.js
+++ src/renderGraphiQL.js
@@ -65,6 +65,7 @@ add "&raw" to the end of the URL within a browser.
     }
   </style>
   <link href="//cdn.jsdelivr.net/npm/graphiql@${GRAPHIQL_VERSION}/graphiql.css" rel="stylesheet" />
+  <script src="//cdn.jsdelivr.net/es6-promise/4.0.5/es6-promise.auto.min.js"></script>
   <script src="//cdn.jsdelivr.net/fetch/0.9.0/fetch.min.js"></script>
   <script src="//cdn.jsdelivr.net/react/15.4.2/react.min.js"></script>
   <script src="//cdn.jsdelivr.net/react/15.4.2/react-dom.min.js"></script>
