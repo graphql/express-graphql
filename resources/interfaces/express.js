e3f4740cb0e1eb217bae8f783dc62ccabec9218e
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-02-18 18:49:05 +0200

    Update prettier

--- resources/interfaces/express.js
+++ resources/interfaces/express.js
@@ -2,15 +2,15 @@
 /* Flow declarations for express requests and responses */
 /* eslint-disable no-unused-vars */
 declare class Request {
-  method: String,
-  body: Object,
-  query: Object,
+  method: String;
+  body: Object;
+  query: Object;
 }
 
 declare class Response {
-  status: (code: Number) => Response,
-  set: (field: String, value: String) => Response,
-  send: (body: String) => void,
-  end: (body: Buffer) => void,
-  json: (body: Object) => void,
+  status: (code: Number) => Response;
+  set: (field: String, value: String) => Response;
+  send: (body: String) => void;
+  end: (body: Buffer) => void;
+  json: (body: Object) => void;
 }
