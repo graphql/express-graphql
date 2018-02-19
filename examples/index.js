fc44076e2c0fc746cd59e1731f53b086fa8c8923
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-02-19 11:55:43 +0200

    Add example server. Useful to manually test GraphiQL

---
+++ examples/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import graphqlHTTP from '../src/';
+import { buildSchema } from 'graphql';
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => 'Hello world!',
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
