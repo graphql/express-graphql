3a209336ebab1d8f44c4c3ee10bb92f3a6d5d676
Enzo Ferey <enzo.ferey@chance.co>
2020-05-19 20:28:14 +0200

    When things are correct, don't make them wrong.

--- src/index.js
+++ src/index.js
@@ -363,7 +363,7 @@ function graphqlHTTP(options: Options): Middleware {
         // Note: Information about the error itself will still be contained in
         // the resulting JSON payload.
         // https://graphql.github.io/graphql-spec/#sec-Data
-        if (response.statusCode === 200 && result && !result.data) {
+        if (response.statusCode !== 200 && result && !result.data) {
           response.statusCode = 500;
         }
         // Format any encountered errors.
