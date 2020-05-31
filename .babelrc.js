ac36170528774a2f4699f92199b53df45e7d2ee7
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2020-06-01 01:41:48 +0300

    Switch to JSON format for `babelrc` file

--- .babelrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: [
-    './resources/load-staticly-from-npm.js',
-    '@babel/plugin-transform-flow-strip-types',
-    '@babel/plugin-transform-modules-commonjs',
-  ],
-};
